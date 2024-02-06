46a6c6543becb923f2f7c770c82113ab3e0de576
heshamalmosawi <hishamalmosawii@gmail.com>
2024-02-06 18:50:15 +0300

    fixed structs and more

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 
@@ -12,15 +13,13 @@ func main() {
 	if len(os.Args) != 2 {
 		log.Fatal("Incorrect format. \nUsage: go run . <filename>")
 	}
-	testWork()
-	// file, err := os.ReadFile(os.Args[1])
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// contentArr := strings.Split(string(file), "\n")
-	// pkg.ProcessData(contentArr)
-
-	// fmt.Println(temp)
+	// testWork() // Activate this and deactivate the rest of main if you want to test a hard coded example
+	file, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	contentArr := strings.Split(string(file), "\n")
+	pkg.ProcessData(contentArr)
 }
 
 func testWork(){
