35036d525ee43c891bac7e2af92fa025b2de6561
heshamalmosawi <hishamalmosawii@gmail.com>
2024-02-09 21:51:18 +0300

    minor change to Print & AddConnection, also log.fatal instead of print

--- lemin/structsAndMethods.go
+++ lemin/structsAndMethods.go
@@ -3,50 +3,53 @@ package pkg
 import (
 	"errors"
 	"fmt"
+	"log"
+	"slices"
 )
 
 type Graph struct {
-	Vertices  []*Vertix
+	Vertices  []*vertex
 	startRoom string
 	endRoom   string
 	ants      int
 }
-type Vertix struct {
+type vertex struct {
 	key         string
-	Connections []*Vertix
+	Connections []*vertex
 }
 
-func (g *Graph) AddVertix(key string) error {
+func (g *Graph) Addvertex(key string) error {
 	if g.Contains(key) {
-		err := fmt.Sprintf("Vertix %s already exists", key)
+		err := fmt.Sprintf("vertex %s already exists", key)
 		return errors.New(err)
 	}
-	g.Vertices = append(g.Vertices, &Vertix{key: key})
+	g.Vertices = append(g.Vertices, &vertex{key: key})
 	return nil
 }
 
 func (g *Graph) AddConnection(from, to string) {
-	Vfrom, err := g.getVertix(from)
+	Vfrom, err := g.getvertex(from)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	Vto, err := g.getVertix(to)
+	Vto, err := g.getvertex(to)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
+	}
+	if slices.Contains(Vfrom.Connections, Vto) || slices.Contains(Vto.Connections, Vfrom) {
+		log.Fatal("ERROR: Two rooms can't be connected with more than two lines.")
 	}
 	Vfrom.Connections = append(Vfrom.Connections, Vto)
 	Vto.Connections = append(Vto.Connections, Vfrom)
 }
 
-func (g *Graph) getVertix(key string) (*Vertix, error) {
+func (g *Graph) getvertex(key string) (*vertex, error) {
 	for _, v := range g.Vertices {
 		if v.key == key {
 			return v, nil
 		}
 	}
-	return nil, errors.New("no vertix exists")
+	return nil, errors.New("no vertex exists")
 }
 
 func (g *Graph) Contains(key string) bool {
@@ -59,8 +62,9 @@ func (g *Graph) Contains(key string) bool {
 }
 
 func (g *Graph) Print() {
+	fmt.Println("Ants:", g.ants, "Start:", g.startRoom, "End:", g.endRoom)
 	for _, v := range g.Vertices {
-		fmt.Printf("Vertix key:" + v.key)
+		fmt.Printf("vertex key:" + v.key)
 		fmt.Print("\tIts connections: ")
 		for _, connection := range v.Connections {
 			fmt.Print(connection.key, " ")
