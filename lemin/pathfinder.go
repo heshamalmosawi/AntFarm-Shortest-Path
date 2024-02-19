b668cfd7ed6a8c54ec365418002fda2b324c7052
aliebrahim <alikebrahim@me.com>
2024-02-19 18:54:02 +0300

    Path sorting implemented

--- lemin/pathfinder.go
+++ lemin/pathfinder.go
@@ -1,6 +1,8 @@
 package lemin
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func (g *Graph) PathFinder() {
 	paths := make([][]string, 0)
@@ -10,6 +12,11 @@ func (g *Graph) PathFinder() {
 		return
 	}
 	g.findPaths(startVertex, []string{g.startRoom}, &paths)
+	paths = sort(paths)
+	fmt.Println("\nShortest Paths:")
+	for _, path := range paths {
+		fmt.Printf("Path through room: %v ==> Path:%v\n", path[1], path) // Temporary check for paths
+	}
 }
 
 // findStartVertex returns the vertex for the start room.
@@ -47,4 +54,65 @@ func contains(slice []string, item string) bool {
 		}
 	}
 	return false
+}
+
+func FindShortestPath(paths [][]string) [][]string {
+	var sPaths [][]string
+	for _, p := range paths {
+		var tmp [][]string
+		room := p[1]
+		tmp = append(tmp, p)
+		for _, p2 := range paths {
+			if p2[1] == room {
+				tmp = append(tmp, p2)
+			}
+		}
+		// fmt.Printf("\nat index %d path:\n %v\n\n", i, tmp)
+		sPaths = append(sPaths, findShortest(tmp))
+	}
+	return sPaths
+}
+func findShortest(paths [][]string) []string {
+	var s []string
+	s = paths[0]
+	for _, p := range paths {
+		if len(p) < len(s) {
+			s = p
+		}
+	}
+	return s
+}
+
+func sort(paths [][]string) [][]string {
+	if len(paths) <= 1 {
+		return paths
+	}
+	pivot := partition(paths)
+	lower := sort(paths[:pivot])
+	higher := sort(paths[pivot+1:])
+	var answer [][]string
+	answer = append(answer, lower...)
+	answer = append(answer, paths[pivot])
+	answer = append(answer, higher...)
+	return answer
+}
+
+func partition(paths [][]string) int {
+	pivot := len(paths[0])
+	i, j := 1, len(paths)-1
+
+	for i <= j {
+		for i <= j && len(paths[i]) <= pivot {
+			i++
+		}
+		for i <= j && len(paths[j]) >= pivot {
+			j--
+		}
+		if i <= j {
+			paths[i], paths[j] = paths[j], paths[i]
+		}
+	}
+	paths[0], paths[j] = paths[j], paths[0]
+	return j
+
 }
