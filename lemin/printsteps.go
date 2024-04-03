a76534cd0352c81d84c0773f44d09796a89560f1
heshamalmosawi <hishamalmosawii@gmail.com>
2024-04-03 13:16:14 +0300

    fixed minor issue of first room being last room

--- lemin/printsteps.go
+++ lemin/printsteps.go
@@ -10,34 +10,25 @@ func (g *Graph) walkIt(paths [][]string) {
 	queue := g.makeQueue(paths)
 	// Creating antsWithNum to have all ants numbered/labeled.
 	antsWithNum := make(map[string]string, g.ants)
-	for i := 0; i < g.ants; i++ {
+	for i := 1; i <= g.ants; i++ {
 		antName := "L" + strconv.Itoa(i)
 		antsWithNum[antName] = ""
 	}
 
 	// the walking loop
-	antsRemaining := g.ants + 1
-	for antsRemaining >= 0 {
-		g.stepForward(paths, queue, antsWithNum)
-		antsRemaining--
+	antsRemaining := g.ants
+	for antsRemaining > 0 {
+		antsRemaining = g.stepForward(paths, queue, antsWithNum, antsRemaining)
+
 	}
 	fmt.Println()
-}
-
-/* This function reduces the number of ants from the queue, this is to be used after ants take a step forward. */
-func reduceQueueNum(queue *[]int) {
-	for i := range *queue {
-		if (*queue)[i] > 0 {
-			(*queue)[i]--
-		}
-	}
 }
 
 /*
 This method simulates the ants stepping a step forward to the next room and prints each step.
 It starts from the end and iterates backward to the start so it can free up room to other ants.
 */
-func (g Graph) stepForward(paths [][]string, queue []int, antsWithNum map[string]string) {
+func (g Graph) stepForward(paths [][]string, queue []int, antsWithNum map[string]string, antsRemaining int) int {
 
 	needforNewLine := false // for convenience
 
@@ -46,6 +37,7 @@ func (g Graph) stepForward(paths [][]string, queue []int, antsWithNum map[string
 		node, _ := g.GetVertex(paths[i][len(paths[i])-2])
 		if node.antInRoom[0] != "" { // if second to last room is not empty
 			fmt.Print(node.antInRoom[0], "-", paths[i][len(paths[i])-1], " ")
+			antsWithNum[node.antInRoom[0]] = "end"
 			node.antInRoom[0] = "" // freeing up the room
 			needforNewLine = true
 		}
@@ -75,21 +67,32 @@ func (g Graph) stepForward(paths [][]string, queue []int, antsWithNum map[string
 			node.antInRoom[0] = temp
 			antsWithNum[temp] = node.antInRoom[0]
 			fmt.Print(node.antInRoom[0], "-", node.key, " ")
+			antsRemaining--
+			queue[i]--
+			if paths[i][1] == g.endRoom { // if the first room is also the end room, then we need to free it up immediately
+				antsWithNum[node.antInRoom[0]] = "end"
+				node.antInRoom[0] = "" // freeing up the room
+			}
 			needforNewLine = true
 		}
 	}
-	reduceQueueNum(&queue)
+	// reduceQueueNum(&queue)
 	if needforNewLine {
 		fmt.Println()
 	}
+	return antsRemaining
 }
 
 /* This function finds an ant that is waiting for its chance to step in. If none found it will return an empty string. */
 func findNextFreeWaitingAnt(antsWithNum map[string]string) string {
-	for key, room := range antsWithNum {
-		if room == "" {
-			return key
+
+	for i := 1; i <= len(antsWithNum); i++ {
+		lnum := fmt.Sprintf("L%d", i)
+		// fmt.Println(lnum)
+		if antsWithNum[lnum] == "" {
+			return lnum
 		}
 	}
+
 	return ""
 }
